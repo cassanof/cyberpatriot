f2681433d4e379dd0c539e6797d8a662dcccc137
elleven11 <elleven11@github.com>
2020-11-16 11:44:40 -0500

    made usermanagement better

--- scripts/usermanagement.sh
+++ scripts/usermanagement.sh
@@ -1,14 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-	for x in `cut -d: -f1 /etc/passwd`
-	do
-		read -p "Is $x a valid user?[y/n]: " a
-		if [ $a = n ] || [ $a = "" ];
-		then
-			mv /home/$x /home/dis_$x
-			sed -i -e "/$x/ s/^#*/#/" /etc/passwd
-			sleep 1
-    else
-      echo "$x" >> existingusers
-		fi
-	done
+_l="/etc/login.defs"
+_p="/etc/passwd"
+
+## get min UID limit ##
+MINUID=$(grep "^UID_MIN" $_l)
+ 
+## get max UID limit ##
+MAXUID=$(grep "^UID_MAX" $_l)
+
+
+for x in `
+    awk -F':' -v "min=${MINUID##UID_MIN}" -v "max=${MAXUID##UID_MAX}" '{ if ( $3 >= min && $3 <= max  && $7 != "/sbin/nologin" ) print $0 }' "$_p" \
+      | cut -d: -f1 -
+  `
+do
+  read -p "Is $x a valid user?[y/n]: " a
+  if [ $a = n ];
+  then
+    mv /home/$x /home/dis_$x
+    sed -i -e "/$x/ s/^#*/#/" /etc/passwd
+    sleep 1
+  else
+    echo "$x" >> ./existingusers.txt
+  fi
+done
