5bf72b8f38f112d9bec23fcfa869da576b40698a
elleven11 <elleven11@github.com>
2020-11-16 11:20:00 -0500

    .

--- scripts/removeusers.sh
+++ scripts/removeusers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-	for x in `cat users`
+	for x in `cut -d: -f1 /etc/passwd`
 	do
 		read -p "Is $x a valid user?[y/n]: " a
 		if [ $a = n ];
@@ -8,6 +8,7 @@
 			mv /home/$x /home/dis_$x
 			sed -i -e "/$x/ s/^#*/#/" /etc/passwd
 			sleep 1
+    else
+      echo "$x" >> existingusers
 		fi
 	done
-	pause
