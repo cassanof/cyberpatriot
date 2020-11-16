59b522bac4feb7ccce31c4527c7fcf474d901524
elleven11 <elleven11@github.com>
2020-11-16 11:27:04 -0500

    .

--- scripts/usermanagement.sh
+++ scripts/usermanagement.sh
@@ -3,7 +3,7 @@
 	for x in `cut -d: -f1 /etc/passwd`
 	do
 		read -p "Is $x a valid user?[y/n]: " a
-		if [ $a = n ];
+		if [ $a = n ] || [ $a = "" ];
 		then
 			mv /home/$x /home/dis_$x
 			sed -i -e "/$x/ s/^#*/#/" /etc/passwd
